9975bcc471a198aaa68c1145ce0881035a0a1add
Juha Ritakoski <juha.ritakoski@hotmail.com>
2019-12-09 23:39:27 +0200

    Update schedule.js
    updated schedule to be able to handle a shorter command for scheduling, which is more flexible by allowing any day of the week instead of only monday and wednesday.

--- commands/schedule.js
+++ commands/schedule.js
@@ -5,17 +5,28 @@ module.exports.run = async (bot, message, args) => {
     var date = new Date()
     try {
         message.delete()
-    } catch (error) {console.error(error)}
-    switch (args[0]) {
-        case "mon":
-        case "monday":
-            date.setDate(date.getDate() + (1 + 7 - date.getDay()) % 7); break;
-        case "wednesday":
-        case "wed":
-            date.setDate(date.getDate() + (3 + 7 - date.getDay()) % 7); break;
-        default:
-            date = new Date(args[0]); break;
+    } catch (error) { console.error(error) }
+    var week = {
+        "mon": 1, "monday": 1,
+        "tue": 2, "tuesday": 2,
+        "wed": 3, "wednesday": 3,
+        "thu": 4, "thursday": 4,
+        "fri": 5, "friday": 5,
+        "sat": 6, "saturday": 6,
+        "sun": 7, "sunday": 7
     }
+    var day = 0
+    if (week[args[0]]) {
+        day = week[args[0]]
+    } else if (week[message_copy.content.slice(1)]) {
+        day = week[message_copy.content.slice(1)]
+    }
+    if (day) {
+        date.setDate(date.getDate() + (day + 7 - date.getDay()) % 7)
+    } else {
+        date = new Date(args[0])
+    }
+
 
     let roles = ""
     for (const role in bot.roles[message_copy.guild.id]) {
@@ -33,5 +44,12 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-    name: "schedule"
+    name: "schedule",
+    mon: "mon",
+    tue: "tue",
+    wed: "wed",
+    thu: "thu",
+    fri: "fri",
+    sat: "sat",
+    sun: "sun"
 }
