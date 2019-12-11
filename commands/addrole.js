574a536926c220e4a84ad82f33e8077ad28df53f
Juha Ritakoski <juha.ritakoski@hotmail.com>
2019-12-11 18:51:19 +0200

    Update addrole.js
    Added a check to see if a role already existed and gave it a proper response

--- commands/addrole.js
+++ commands/addrole.js
@@ -14,6 +14,21 @@ module.exports.run = async (bot, message, args) => {
         return
     }
 
+    if (bot.roles[message.guild.id][args[0]]) {
+        try {
+            let role = args[0]
+            let emoji = bot.roles[message.guild.id][role]
+            let custom_emoji = bot.emojis.find(emoji => emoji.name === bot.roles[message.guild.id][role])
+            if (custom_emoji) emoji = custom_emoji
+            var sent = await message.channel.send(`Role ${role} already exists with emoji ${emoji}, choose another name for the role or remove old role first.`)
+        } catch (error) {
+            console.error(error)
+        } finally {
+            await sent.delete(Config.deletion_timer)
+        }
+        return
+    }
+
     message_copy.channel.send(`⚔️ What emoji should correspond to ${args[0]}`)
 
 }
