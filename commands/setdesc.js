4664d95ec11e817d124d1a83112cff8fb391cf4b
Juha Ritakoski <juha.ritakoski@hotmail.com>
2019-12-15 00:04:27 +0200

    Added error and confirmation message for !setdesc
    A !setdesc command with no <description> answers with an error message. A successful one will relay that the setting was successful.

--- commands/setdesc.js
+++ commands/setdesc.js
@@ -1,4 +1,5 @@
 const Description = require("../description.json")
+const Config = require("../config.json")
 const fs = require("fs")
 
 module.exports.run = async (bot, message, args) => {
@@ -6,11 +7,24 @@ module.exports.run = async (bot, message, args) => {
     try {
         await message.delete()
     } catch (error) { console.error(error) }
-    Description[message_copy.guild.id] = message_copy.content.slice(9)
+    var desc = message_copy.content.slice(9)
+    if (desc == "") {
+        try {
+            var sent = await message_copy.channel.send(`⚔️ Default description can't be empty.`)
+            await sent.delete(Config.deletion_timer)
+        } catch (error) { console.error(error) }
+        return
+    }
+    Description[message_copy.guild.id] = desc
 
     fs.writeFile("../description.json", JSON.stringify(Description, null, 4), async (error) => {
         if (error) console.error(error)
     })
+
+    try {
+        var sent = await message_copy.channel.send(`⚔️ Default description has been changed to "${desc}"`)
+        await sent.delete(Config.deletion_timer)
+    } catch (error) { console.error(error) }
 }
 
 module.exports.help = {
