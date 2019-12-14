04d63887c911257d20b047a5064a5714d1f1631f
Juha Ritakoski <juha.ritakoski@hotmail.com>
2019-12-14 23:41:34 +0200

    Added possibility to add custom description to schedule creation
    Schedules can now be created with a custom description with !schedule <date> <description> and  !mon <description>. The description can be omitted to use the default time from Config.json.

--- commands/schedule.js
+++ commands/schedule.js
@@ -16,15 +16,35 @@ module.exports.run = async (bot, message, args) => {
         "sun": 7, "sunday": 7
     }
     var day = 0
-    if (week[args[0]]) {
+
+    if (message_copy.content.startsWith("!schedule")) {
         day = week[args[0]]
-    } else if (week[message_copy.content.slice(1)]) {
-        day = week[message_copy.content.slice(1)]
+        var description = message_copy.content.slice(10).split(/(?<=^\S+)\s/)[1]
+        if (!description) {
+            description = Config.time[message_copy.guild.id]
+        }
+    } else {
+        day = week[message_copy.content.split(" ")[0].slice(1)]
+        var description = message_copy.content.split(/(?<=^\S+)\s/)[1]
+        if (!description) {
+            description = Config.time[message_copy.guild.id]
+        }
     }
+
     if (day) {
+        var date = new Date()
         date.setDate(date.getDate() + (7 - date.getDay()) % 7 + day)
     } else {
         date = new Date(args[0])
+    }
+
+    if (!date instanceof Date || isNaN(date.getTime())) {
+        var error_msg = "Usage of command is !schedule <date> <description>. Use !help for more information."
+        try {
+            var sent = await message_copy.channel.send(error_msg)
+            await sent.delete(Config.deletion_timer)
+        } catch (error) { console.error(error) }
+        return
     }
 
     let roles = ""
@@ -36,7 +56,7 @@ module.exports.run = async (bot, message, args) => {
         }
     }
     date = date.toDateString()
-    let schedule = `> __**${date}**__\n> **${Config.time[message_copy.guild.id]}**\n Sign up by clicking one of the corresponding reactions! \n[0/10]\`\`\`${roles} \nBackups: \n---------------\nCan't make it: \`\`\``
+    let schedule = `> __**${date}**__\n> **${description}**\n Sign up by clicking one of the corresponding reactions! \n[0/10]\`\`\`${roles} \nBackups: \n---------------\nCan't make it: \`\`\``
     try {
         await message_copy.channel.send(schedule)
     } catch (error) { console.error(error) }
